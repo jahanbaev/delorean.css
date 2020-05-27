97b514067d17ff15ff79883db0a164165d1c9ef7
jahanbaev <65107752+jahanbaev@users.noreply.github.com>
2020-05-27 07:19:17 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+    "beforeend",
+    "<link rel=\"stylesheet\" href=\"font-style.css\" />");
+	
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+    "beforeend",
+    "<link rel=\"stylesheet\" href=\"font-size.css\" />");
+	
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+    "beforeend",
+    "<link rel=\"stylesheet\" href=\"font-color.css\" />");
